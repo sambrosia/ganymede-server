340d45d81fd8cdb02d6f0cf5bddb9b0fd2a2b09f
sambrosia <samuelcwoodruff@gmail.com>
2017-11-25 23:03:48 -0700

    Improve auth middleware
    Move data to request body, and extract some functionality into separate
    middlewares.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,39 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const jwt = require('jsonwebtoken')
 const rethink = require('rethinkdb')
 const axios = require('axios')
 const parseRSS = require('./parseRSS.js')
 const CORS = require('./CORS.js')
 
-const app = express()
 const invalidTokens = new Set()
 
+// Responds with an error if the provided token is blacklisted
+// Does nothing if there is no token or the token is not blacklisted
+function hasValidToken(req, res, next) {
+  if (req.body.authToken && invalidTokens.has(req.body.authToken)) {
+    res.status(403).json({
+      name: 'JsonWebTokenError',
+      message: 'jwt invalidated'
+    })
+  } else next()
+}
+
+// Blacklists the token
+function invalidateToken(req, res, next) {
+  if (req.body.authToken) invalidTokens.add(req.body.authToken)
+  next()
+}
+
+// Sets the req.user for future middlewares to check auth status
+// TODO: Persist user document in DB
+function setUser(req, res, next) {
+  if (req.body.authToken)
+    req.user = jwt.verify(req.body.authToken, 'supersecret')
+  next()
+}
+
+// Connect to RethinkDB
 let connection = null
 rethink
   .connect({ host: 'localhost', port: 28015 })
@@ -16,64 +42,37 @@ rethink
   })
   .catch(error => console.log(error))
 
-// Enable CORS middleware
+// Create Express app
+const app = express()
 app.use(CORS)
+app.use(bodyParser.json())
 
 // Get temporary login token for given user
-app.post('/login/:email', (req, res) => {
-  const token = jwt.sign({ id: req.params.email }, 'supersecret', {
+app.post('/login', (req, res) => {
+  const token = jwt.sign({ email: req.body.email }, 'supersecret', {
     expiresIn: '5m'
   })
 
   // TODO: Send login link w/ this temp token to email
   res.json('Check your email for your login link')
 
-  console.log(`\n${req.ip} requested login for ${req.params.email}`)
+  console.log(`\n${req.ip} requested login for ${req.body.email}`)
   console.log(`login url: ${req.get('origin')}/auth/${token}`)
 })
 
 // Get longer-term token from temp token
-app.post('/auth/:jwt', async (req, res) => {
-  // Make sure the provided token is not blacklisted
-  if (invalidTokens.has(req.params.jwt)) {
-    res.status(403).json({
-      name: 'JsonWebTokenError',
-      message: 'jwt invalidated'
-    })
-    return
-  }
-
-  // Blacklist the token to prevent further logins using it
-  invalidTokens.add(req.params.jwt)
-
+// TODO: Prevent use as login token?
+app.post('/auth', hasValidToken, setUser, invalidateToken, (req, res) => {
   try {
-    // Verify the login token's signature
-    const payload = jwt.verify(req.params.jwt, 'supersecret')
-
-    // Fetch or create the user document
-    let user = await rethink
-      .table('users')
-      .get(payload.id)
-      .run(connection)
-
-    if (user === null) {
-      user = { id: payload.id }
-      rethink
-        .table('users')
-        .insert(user)
-        .run(connection)
-        .catch(error => console.log(error))
-    }
-
     // Create new token for actual authentication
-    const token = jwt.sign(user, 'supersecret', {
+    const token = jwt.sign({ email: req.user.email }, 'supersecret', {
       expiresIn: '7d'
     })
 
     // Send new token to client
     res.json(token)
 
-    console.log(`\n${req.ip} logged in as ${user.id}`)
+    console.log(`\n${req.ip} logged in as ${req.user.email}`)
   } catch (error) {
     res.status(500).json(error)
     console.log(error)
